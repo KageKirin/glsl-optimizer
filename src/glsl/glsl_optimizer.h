90abc6a9e887ce8b936a210ccbc89c20dc952507
Christian Helmich <kagekirin@gmail.com>
2013-04-17 11:55:43 +0900

    added GSOAPI declspecs for dllexport/dllimport

--- src/glsl/glsl_optimizer.h
+++ src/glsl/glsl_optimizer.h
@@ -2,6 +2,14 @@
 #ifndef GLSL_OPTIMIZER_H
 #define GLSL_OPTIMIZER_H
 
+#if defined(GSO_BUILD_DLL)
+#define GLOAPI	__declspec(dllexport)
+#elif defined(GSO_LOAD_DLL)
+#define GLOAPI	__declspec(dllimport)
+#else
+#define GLOAPI	
+#endif
+
 /*
  Main GLSL optimizer interface.
  See ../../README.md for more instructions.
@@ -31,19 +39,19 @@ enum glslopt_shader_type {
 
 // Options flags for glsl_optimize
 enum glslopt_options {
+	kGlslOptionNone = 0,
 	kGlslOptionSkipPreprocessor = (1<<0), // Skip preprocessing shader source. Saves some time if you know you don't need it.
 	kGlslOptionNotFullShader = (1<<1), // Passed shader is not the full shader source. This makes some optimizations weaker.
 };
 
-glslopt_ctx* glslopt_initialize (bool openglES);
-void glslopt_cleanup (glslopt_ctx* ctx);
+GLOAPI glslopt_ctx* glslopt_initialize (bool openglES);
+GLOAPI void glslopt_cleanup (glslopt_ctx* ctx);
 
-glslopt_shader* glslopt_optimize (glslopt_ctx* ctx, glslopt_shader_type type, const char* shaderSource, unsigned options);
-bool glslopt_get_status (glslopt_shader* shader);
-const char* glslopt_get_output (glslopt_shader* shader);
-const char* glslopt_get_raw_output (glslopt_shader* shader);
-const char* glslopt_get_log (glslopt_shader* shader);
-void glslopt_shader_delete (glslopt_shader* shader);
-
+GLOAPI glslopt_shader* glslopt_optimize (glslopt_ctx* ctx, glslopt_shader_type type, const char* shaderSource, unsigned options);
+GLOAPI bool glslopt_get_status (glslopt_shader* shader);
+GLOAPI const char* glslopt_get_output (glslopt_shader* shader);
+GLOAPI const char* glslopt_get_raw_output (glslopt_shader* shader);
+GLOAPI const char* glslopt_get_log (glslopt_shader* shader);
+GLOAPI void glslopt_shader_delete (glslopt_shader* shader);
 
 #endif /* GLSL_OPTIMIZER_H */
